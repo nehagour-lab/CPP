e788fb18afef49f9f3614ed1b40236bc82007d86
NEHA GOUR <77980429+nehagour-lab@users.noreply.github.com>
2023-07-31 20:03:41 -0700

    Add files via upload

---
+++ Fortune_teller_app_Updated_version.cpp
@@ -0,0 +1,39 @@
+int main () {
+#include<iostream>
+#include<ctime>
+using namespace std;    
+int main () {
+
+bool end {false};
+    srand (time(0));
+    const size_t namelength {15};
+    char name[namelength]{};
+    cout<<"Whats your name ?";
+    cin.getline(name,namelength);
+    char prediction [][100]{
+        "a lot of kinds running in the backyard!",
+        "a lot of empty beer bootles on your work table.",
+        "you Partying too much with kids wearing weird clothes.",
+        "you running away from something really scary",
+        "clouds gathering in the sky and an army standing ready for war",
+        "dogs running around in a deserted empty city",
+        "a lot of cars stuck in a terrible traffic jam",
+        "you sitting in the dark typing lots of lines of code on your dirty computer",
+        "you yelling at your boss. And oh no! You get fired!",
+        "you laughing your lungs out. I've never seen this before." ,
+        "Uhm , I don't see anything!"
+    };
+    while(!end){
+        cout<<"Hey "<< name << "  I see  that\n";
+        size_t rand_num = static_cast<size_t>(rand()% size(prediction));
+        cout<<prediction[rand_num]<<"\n";
+
+        cout<<"Do you want anything else? [Y | N]\n";
+        char go_on;
+        cin>>go_on;
+        end = ((go_on == 'Y') || (go_on == 'y'))?false:true;
+         
+    }
+}
+
+}
---
+++ Multi_dimensional_Char_array.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+// int main() {
+//     const size_t Name_length {15};
+//     char Name [][Name_length]{
+//                {'N', 'E','H','A'},
+//                {'A','B','C', 'D'}
+//     };
+
+//     for (size_t i{0}; i<size(Name); ++i){
+//         for(size_t j{0}; j<size(Name[i]); ++j){ 
+//              cout<<Name[i][j];
+//          } 
+//          cout<<" \n";
+//     }
+// }
+
+// int main() {
+//     const size_t Name_length {15};
+//     char Name [][Name_length]{
+//                 "HAPPY",
+//                 "WORLD"
+//     };
+
+//     for (size_t i{0}; i<size(Name); ++i){ 
+//              cout<<i+1 <<". [ "<<Name[i]<<" ]\n";
+//          } 
+// }
