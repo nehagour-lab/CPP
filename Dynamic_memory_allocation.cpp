d5b8354aa0050feda1021b4ad98f0a674a20d937
NEHA GOUR <77980429+nehagour-lab@users.noreply.github.com>
2023-08-05 13:44:38 -0700

    Add files via upload

---
+++ Dynamic_memory_allocation.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main (){
+
+// int *p_number {nullptr};
+// p_number = new int {25};
+// cout<<"*p_number: "<<*p_number<<"\n";
+// cout<<"&p_number: "<<&p_number<<"\n";
+// delete p_number;
+// p_number =nullptr;
+// p_number = new int(100);
+
+// cout<<"*p_number: "<<*p_number<<"\n";
+// cout<<"&p_number: "<<&p_number<<"\n";
+
+// delete p_number;
+// p_number = nullptr;
+// cout<<"Program not crashed\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
